9e375c4f9cda88bf7506ebdd8865de9ad781d36e
ReubenXuereb <56310716+ReubenXuereb@users.noreply.github.com>
2024-10-28 16:08:02 +0100

    First commit

---
+++ src/main/java/com/example/Yieldstreeet/YieldstreetTask/YieldstreetTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.Yieldstreeet.YieldstreetTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YieldstreetTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YieldstreetTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Yieldstreeet/YieldstreetTask/YieldstreetTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Yieldstreeet.YieldstreetTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YieldstreetTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
